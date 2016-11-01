10e0791f932699412ce04dfd6daae79da5a1098b
Adam Greenberg <agreenberg@thehonestcompany.com>
2016-10-31 23:01:19 -0700

    Creating the code for the web socket to interact with the frontend

--- components/App.jsx
+++ components/App.jsx
@@ -14,9 +14,9 @@ class App extends Component{
   }
   componentDidMount(){
     let ws = this.ws = new WebSocket('ws://echo.websocket.org');
-    ws.onmessage = this.message.bind(this);
-    ws.onopen = this.open.bind(this);
-    ws,onclose = this.close.bind(this);
+    // ws.onmessage = this.message.bind(this);
+    // ws.onopen = this.open.bind(this);
+    // ws,onclose = this.close.bind(this);
   }
   message(e) {
     const event = JSON.parse(e.data);
